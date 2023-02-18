3d98d86ef8b1bf035d08051b93f8fbcb2c82bae5
Gustavo Sales <gustavo@gsales.io>
2023-02-18 20:16:01 -0300

    chore: add SCSS convert + render document style (empty)

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,16 @@
 const gulp = require('gulp');
 const gulpUglify = require('gulp-uglify');
-const removeEmptyLines = require('gulp-remove-empty-lines');
-const through = require('through2');
+const gulpSass = require('gulp-sass')(require('sass'));
 
-function removeStylesWhiteSpaces() {
+function scssToCss() {
   return gulp
-    .src('dist/**/*.styles.js')
-    .pipe(
-      (() => {
-        return through.obj((file, encoding, callback) => {
-          if (file.isNull()) {
-            return callback(null, file);
-          }
-
-          file.contents = Buffer.from(
-            (() => {
-              return file.contents
-                .toString()
-                .replace(/(\\r\\n|\\n|\\r)/gm, '')
-                .replace(/\s+/g, ' ')
-                .replace(/" /g, '"')
-                .replace(/ "/g, '"');
-            })()
-          );
-
-          callback(null, file);
-        });
-      })()
-    )
-    .pipe(gulp.dest('dist/'));
+    .src('./**/*.scss')
+    .pipe(gulpSass().on('error', gulpSass.logError))
+    .pipe(gulp.dest((file) => file.base));
 }
 
 function uglify() {
   return gulp.src('dist/**/*.js').pipe(gulpUglify()).pipe(gulp.dest('dist/'));
 }
 
-exports.postBuild = gulp.series(removeStylesWhiteSpaces, uglify);
+exports.postBuild = gulp.series(scssToCss, uglify);
